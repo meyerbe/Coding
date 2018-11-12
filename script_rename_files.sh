fb76e7bef072da172fb6405eaf7c3370fb66d0df
Bettina Meyer <meyerbe@condensation.biocmplx.nbi.dk>
2018-11-12 14:51:22 +0100

    pritn statements

--- script_rename_files.sh
+++ script_rename_files.sh
@@ -2,13 +2,12 @@
 
 
 # Input path to data
-#read -p "Path to DARWIN data:" path_darwin
+read -p "Path to data: " path_root
 #path_root=/Users/bettinameyer/polybox/ClimatePhysics/Copenhagen/Projects/LES_ColdPool/double_2D_nonoise/Out_CPDry_double_2D_dTh10K
 #path_in=$path_root/fields_k120/
-path_root=/Users/bettinameyer/polybox/ClimatePhysics/Copenhagen/Projects/LES_ColdPool/triple_3D_noise/Out_CPDry_triple_dTh3K
-#path_in=$path_root/fields/
-path_in=$path_root/test_dir/
-path_out=$path_root/fields_renamed/
+#path_root=/Users/bettinameyer/polybox/ClimatePhysics/Copenhagen/Projects/LES_ColdPool/triple_3D_noise/Out_CPDry_triple_dTh3K
+path_in=$path_root/fields
+path_out=$path_root/fields_renamed
 
 echo "renaming files in: $path_in"
 mkdir -p $path_out  # only create directory if not existing
@@ -22,16 +21,12 @@ echo "output in: $path_out"
 for entry in "$path_in"/*.nc
   do
   file_name=$(basename $entry)
-  #echo $entry
   echo $file_name
   declare -i chrlen=${#file_name}
-  echo $chrlen
   if [ $chrlen \< 6 ]
     then
     echo "below 100"
     new_name="10000"$file_name
-    #echo $new_name
-    #cp $path_in/$file_name $path_out/$new_name
   elif [ $chrlen \< 7 ]
     then
     echo "below 1000"
